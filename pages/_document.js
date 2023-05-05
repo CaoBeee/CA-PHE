ebbeab33780fe891baf7171e82b50298e9b54e31
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2023-05-05 14:07:12 -0700

    header and navbar

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html>
-      <Head>
-		<link rel="stylesheet" href="../styles/styles.css" />
-		<meta charset="UTF-8" />
-	  </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html>
+			<Head>
+				<link
+					rel='stylesheet'
+					href='../styles/styles.css'
+				/>
+				<meta charset='UTF-8' />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,8 @@
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import logo from '../public/images/ca phe mock client profile transparent.png';
-import facebookLogo from '../public/images/Facebook Logo.png';
-import instagramLogo from '../public/images/Instagram Logo.png';
-import twitterLogo from '../public/images/Twitter Logo.png';
-import styles from '../styles/Index.module.css';
+import React from 'react'
+import Home from './home'
 
 const LandingPage = () => {
-    return (
-        <div className="LandingPage">
-            <Image className={styles.LandingPage__Logo} src={logo} alt="logo" />
-            <ul className={styles.LandingPage__navLinks}>
-                <Link href="/Menu">
-                    <button>
-                        Menu
-                    </button>
-                </Link>
-                <Link href="/Seasonal">
-                    <button>
-                        Seasonal Dishes
-                    </button>
-                </Link>
-                <Link href="/Inside">
-                    <button>
-                        Inside the Cafe
-                    </button>
-                </Link>
-                <Link href="/FAQ">
-                    <button>
-                        Delivery FAQ
-                    </button>
-                </Link>
-                <Link href="/About">
-                    <button>
-                        About Us
-                    </button>
-                </Link>
-                <Link href="/Advocacy">
-                    <button>
-                        Our Advocacy
-                    </button>
-                </Link>
-                <div className={styles.socials }>
-                    <Image src={facebookLogo} alt="facebookLogo" />
-                    <Image src={instagramLogo} alt="instagramLogo" />
-                    <Image src={twitterLogo} alt="twitterLogo" />
-                </div>
-            </ul>
-        </div>
-    );
-};
+	return <Home />
+}
 
-export default LandingPage;
+export default LandingPage
