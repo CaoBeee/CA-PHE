2958b7a2c4a366c3c550b73e28f2bf576d7535d2
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2023-05-06 16:43:35 -0700

    instagram bar and draggable gallery

--- components/instagramBar.js
+++ components/instagramBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from '../styles/InstagramBar.module.css'
 import Image from 'next/image'
 import InstagramLogo from '../public/images/InstagramLogo.png'
+import Draggable from 'react-draggable'
 
 const InstagramBar = () => {
 	return (
@@ -17,6 +18,126 @@ const InstagramBar = () => {
 					height={120}
 				/>
 			</div>
+
+			<Draggable
+				className={styles.image}
+				axis='x'
+			>
+				<div className={styles.gallery}>
+					<Image
+						draggable='false'
+						className={styles.image}
+						src='/../public/images/coffee2.jpeg'
+						alt=''
+						width={257}
+						height={216}
+					/>
+					<Image
+						draggable='false'
+						className={styles.image}
+						src='/../public/images/coffee2.jpeg'
+						alt=''
+						width={257}
+						height={216}
+					/>
+					<Image
+						draggable='false'
+						className={styles.image}
+						src='/../public/images/coffee2.jpeg'
+						alt=''
+						width={257}
+						height={216}
+					/>
+					<Image
+						draggable='false'
+						className={styles.image}
+						src='/../public/images/coffee2.jpeg'
+						alt=''
+						width={257}
+						height={216}
+					/>
+					<Image
+						draggable='false'
+						className={styles.image}
+						src='/../public/images/coffee2.jpeg'
+						alt=''
+						width={257}
+						height={216}
+					/>
+					<Image
+						draggable='false'
+						className={styles.image}
+						src='/../public/images/coffee2.jpeg'
+						alt=''
+						width={257}
+						height={216}
+					/>
+					<Image
+						draggable='false'
+						className={styles.image}
+						src='/../public/images/coffee2.jpeg'
+						alt=''
+						width={257}
+						height={216}
+					/>
+					<Image
+						draggable='false'
+						className={styles.image}
+						src='/../public/images/coffee2.jpeg'
+						alt=''
+						width={257}
+						height={216}
+					/>
+					<Image
+						draggable='false'
+						className={styles.image}
+						src='/../public/images/coffee2.jpeg'
+						alt=''
+						width={257}
+						height={216}
+					/>
+					<Image
+						draggable='false'
+						className={styles.image}
+						src='/../public/images/coffee2.jpeg'
+						alt=''
+						width={257}
+						height={216}
+					/>
+					<Image
+						draggable='false'
+						className={styles.image}
+						src='/../public/images/coffee2.jpeg'
+						alt=''
+						width={257}
+						height={216}
+					/>
+					<Image
+						draggable='false'
+						className={styles.image}
+						src='/../public/images/coffee2.jpeg'
+						alt=''
+						width={257}
+						height={216}
+					/>
+					<Image
+						draggable='false'
+						className={styles.image}
+						src='/../public/images/coffee2.jpeg'
+						alt=''
+						width={257}
+						height={216}
+					/>
+					<Image
+						draggable='false'
+						className={styles.image}
+						src='/../public/images/coffee2.jpeg'
+						alt=''
+						width={257}
+						height={216}
+					/>
+				</div>
+			</Draggable>
 		</React.Fragment>
 	)
 }
