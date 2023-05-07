125a5ec250f8ca2acc1a94167291ad53cb0c75eb
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2023-05-06 23:36:50 -0700

    instagram component and draggable image gallery

--- components/instagramBar.js
+++ components/instagramBar.js
@@ -5,138 +5,40 @@ import InstagramLogo from '../public/images/InstagramLogo.png'
 import Draggable from 'react-draggable'
 
 const InstagramBar = () => {
+	const numImages = 50
+	const imagesArr = []
+	for (let i = 0; i < numImages; i++) {
+		imagesArr.push(
+			<Image
+				draggable='false'
+				className={styles.image}
+				src='/../public/images/coffee2.jpeg'
+				alt=''
+				width={257}
+				height={216}
+			/>
+		)
+	}
 	return (
 		<React.Fragment>
 			<div className={styles.container}>
 				<div>
 					<p>Take a peek at the latest updates</p>
 				</div>
-				<Image
-					src={InstagramLogo}
-					alt=''
-					width={120}
-					height={120}
-				/>
+				<a href='https://www.instagram.com'>
+					<Image
+						src={InstagramLogo}
+						alt=''
+						width={120}
+						height={120}
+					/>
+				</a>
 			</div>
-
 			<Draggable
-				className={styles.image}
 				axis='x'
+				bounds={{ right: 0, left: -(numImages - 1) * 100 }}
 			>
-				<div className={styles.gallery}>
-					<Image
-						draggable='false'
-						className={styles.image}
-						src='/../public/images/coffee2.jpeg'
-						alt=''
-						width={257}
-						height={216}
-					/>
-					<Image
-						draggable='false'
-						className={styles.image}
-						src='/../public/images/coffee2.jpeg'
-						alt=''
-						width={257}
-						height={216}
-					/>
-					<Image
-						draggable='false'
-						className={styles.image}
-						src='/../public/images/coffee2.jpeg'
-						alt=''
-						width={257}
-						height={216}
-					/>
-					<Image
-						draggable='false'
-						className={styles.image}
-						src='/../public/images/coffee2.jpeg'
-						alt=''
-						width={257}
-						height={216}
-					/>
-					<Image
-						draggable='false'
-						className={styles.image}
-						src='/../public/images/coffee2.jpeg'
-						alt=''
-						width={257}
-						height={216}
-					/>
-					<Image
-						draggable='false'
-						className={styles.image}
-						src='/../public/images/coffee2.jpeg'
-						alt=''
-						width={257}
-						height={216}
-					/>
-					<Image
-						draggable='false'
-						className={styles.image}
-						src='/../public/images/coffee2.jpeg'
-						alt=''
-						width={257}
-						height={216}
-					/>
-					<Image
-						draggable='false'
-						className={styles.image}
-						src='/../public/images/coffee2.jpeg'
-						alt=''
-						width={257}
-						height={216}
-					/>
-					<Image
-						draggable='false'
-						className={styles.image}
-						src='/../public/images/coffee2.jpeg'
-						alt=''
-						width={257}
-						height={216}
-					/>
-					<Image
-						draggable='false'
-						className={styles.image}
-						src='/../public/images/coffee2.jpeg'
-						alt=''
-						width={257}
-						height={216}
-					/>
-					<Image
-						draggable='false'
-						className={styles.image}
-						src='/../public/images/coffee2.jpeg'
-						alt=''
-						width={257}
-						height={216}
-					/>
-					<Image
-						draggable='false'
-						className={styles.image}
-						src='/../public/images/coffee2.jpeg'
-						alt=''
-						width={257}
-						height={216}
-					/>
-					<Image
-						draggable='false'
-						className={styles.image}
-						src='/../public/images/coffee2.jpeg'
-						alt=''
-						width={257}
-						height={216}
-					/>
-					<Image
-						draggable='false'
-						className={styles.image}
-						src='/../public/images/coffee2.jpeg'
-						alt=''
-						width={257}
-						height={216}
-					/>
-				</div>
+				<div className={styles.gallery}>{imagesArr}</div>
 			</Draggable>
 		</React.Fragment>
 	)
