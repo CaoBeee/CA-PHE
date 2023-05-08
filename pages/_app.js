8fa4cc8d61f57e83c18f368c1ae37c10b13f5375
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2023-05-08 12:08:27 -0700

    hotfix for casing errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 // import Head from 'next/head'
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/styles.css'
-import Layout from '../components/layout';
+import Layout from '../components/Layout'
 
 export default function MyApp({ Component, pageProps }) {
-
-	return (<Layout>
-		<Component {...pageProps} />
-	</Layout>
-		);
+	return (
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
+	)
 }
