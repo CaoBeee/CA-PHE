d1f579415e4eec8fb3de368fbf09f8399359ee7f
Spencer-Breding <Spencerbreding@gmail.com>
2023-05-11 13:45:40 -0700

    Created Shop Page

---
+++ components/shopGallery1.js
@@ -0,0 +1,53 @@
+﻿import React from 'react';
+import styles from '../styles/gallery.module.css';
+
+export default function shopGallery1() {
+    return (
+        <div className={styles.gallery_container}>
+            <div className={styles.gallery_item}>
+                <a href="/Shop">
+                    <img src="https://images.squarespace-cdn.com/content/v1/58e3cd71b8a79bb4b85f2249/1683680734696-2CKUNS49NPLUGYFUJQ2D/Princess_BlkDMH.jpg?format=1500w" alt="" />
+                </a>
+                <p>Cats also have a fascinating social structure. They are known for being solitary creatures, but they can also form strong bonds with other cats and even with humans. Watching a group of cats interact is like observing a tiny society with its own rules and hierarchies.
+
+                    And let's not forget about the health benefits of owning a cat. Studies have shown that spending time with cats can lower stress levels, reduce anxiety and depression, and even lower blood pressure. There's something incredibly soothing about petting a cat and listening to the gentle purring sound they make.</p>
+            </div>
+            <div className={styles.gallery_item}>
+                <a href="/Shop">
+                    <img src="https://nationaltoday.com/wp-content/uploads/2020/10/Global-Cat-Day-1.jpg" alt="" />
+                </a>
+                <p>Plus, cats are just plain cute! From their adorable little noses to their tiny paws, everything about them is endearing. And don't even get me started on the funny positions they like to sleep in or the silly antics they get up to when they're feeling playful.</p>
+            </div>
+            <div className={styles.gallery_item}>
+                <a href="/Shop">
+                    <img src="https://hips.hearstapps.com/hmg-prod/images/large-cat-breed-1553197454.jpg" alt="" />
+                </a>
+                <p>Overall, cats are simply wonderful creatures that bring so much joy and happiness into our lives. They are loyal, loving, and endlessly entertaining. Whether you're a lifelong cat lover or are considering getting a cat for the first time, I can guarantee that they will quickly become an important part of your life.</p>
+            </div>
+            <div className={styles.gallery_item}>
+                <a href="/Shop">
+                    <img src="https://www.marthastewart.com/thmb/_kw9q7qT7lwUbQXIj2KAeYtVY1M=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/yellow-cat-playing-toy-4XFTyv4-KSL8G8VqtTWszB-0322-c641e08159ae4a96a93ba3ebc3c043da.jpg" alt="" />
+                </a>
+                <p>
+                    One of the best things about cats is their independence. They are perfectly content to spend time alone, grooming themselves or napping in a sunny spot. But at the same time, they are also capable of being incredibly affectionate and loving. There's nothing quite like the feeling of a soft, purring cat curled up on your lap.
+                </p>
+            </div>
+            <div className={styles.gallery_item}>
+                <a href="/Shop">
+                    <img src="https://images.squarespace-cdn.com/content/v1/58e3cd71b8a79bb4b85f2249/1683680442432-FF14SIFJWNOPY11I9LCC/AJ_BlkGrayTab.jpg?format=1500w" alt="" />
+                </a>
+                <p>
+                    Another thing I love about cats is their playful nature. Whether they're chasing a toy or stalking a piece of string, watching a cat play is endlessly entertaining. And let's not forget about their incredible agility and acrobatic skills. Seeing a cat leap effortlessly onto a high surface or contort their body in impossible ways is truly a sight to behold.
+                </p>
+            </div>
+            <div className={styles.gallery_item}>
+                <a href="/Shop">
+                    <img src="https://cdn.rescuegroups.org/6685/pictures/animals/19242/19242832/93902467.jpg" alt="" />
+                </a>
+                <p>
+                    Cats are also incredibly intelligent and perceptive. They have a keen sense of observation and can pick up on subtle cues and changes in their environment. And while they may seem aloof at times, they are actually very attuned to their owners' emotions and can be incredibly comforting in times of stress or sadness.
+                </p>
+            </div>
+        </div>
+    );
+}
