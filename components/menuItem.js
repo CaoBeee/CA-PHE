165e175e5620606a25904fb143caffd856df1dcb
Spencer-Breding <Spencerbreding@gmail.com>
2023-06-17 16:49:18 -0700

    Destructured props in menuItem.js

--- components/menuItem.js
+++ components/menuItem.js
@@ -5,16 +5,16 @@ import coldIcon from '../public/images/cold-Icon.png'
 import hotIcon from '../public/images/hot-Icon.png'
 import styles from '../styles/MenuItem.module.css'
 
-export default function MenuItem(props) {
+export default function MenuItem({item_image, name, calories, recommended, cold, hot }) {
 	return (
 		<div className={styles.container}>
-			<Image className={styles.item_image} src={props.item_image }/>
-			<span className={styles.name}>{props.name}</span>
-			<span className={styles.calories}>{props.calories}</span>
+			<Image className={styles.item_image} src={item_image }/>
+			<span className={styles.name}>{name}</span>
+			<span className={styles.calories}>{calories}</span>
 			<div className={styles.icon_container}>
-				{props.recommended && <Image className={styles.icon} src={recommendedIcon} />}
-				{props.cold && <Image className={styles.icon} src={coldIcon} />}
-				{props.hot && <Image className={styles.icon} src={hotIcon} />}
+				{recommended && <Image className={styles.icon} src={recommendedIcon} />}
+				{cold && <Image className={styles.icon} src={coldIcon} />}
+				{hot && <Image className={styles.icon} src={hotIcon} />}
 			</div>
 		</div>
 		)
