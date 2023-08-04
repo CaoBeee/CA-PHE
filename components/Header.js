5d13cc3180b3e5c0536950c8eb9575cf8c6f1555
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2023-08-04 14:23:31 -0700

    Navbar hover effects

--- components/Header.js
+++ components/Header.js
@@ -2,17 +2,22 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import logo from '../public/images/logo_caphe-01.svg'
+import styles from '../styles/Header.module.css'
+import NavBar from './NavBar'
 
-const Header = () => {
+const Header = ({ isSticky }) => {
 	return (
+		<header className={styles.header}>
 			<Link href='/'>
 				<Image
 					src={logo}
-				alt='Logo'
-				height={500}
-				width={500}
+					alt='Logo'
+					height={500}
+					width={500}
 				></Image>
 			</Link>
+			<NavBar isSticky={isSticky}></NavBar>
+		</header>
 	)
 }
 
--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import NavBar from './NavBar.js'
 import Header from './Header.js'
 import Footer from './Footer.js'
-import styles from '../styles/Header.module.css'
 import PropTypes from 'prop-types'
 
 Layout.propTypes = {
@@ -10,29 +9,26 @@ Layout.propTypes = {
 }
 
 export default function Layout({ children }) {
-	const [isSticky, setIsSticky] = useState(false);
+	const [isSticky, setIsSticky] = useState(false)
 
 	useEffect(() => {
-			const handleScroll = () => {
-				const scrollThreshold = 100;
-				const isScrolled = window.pageYOffset > scrollThreshold;
-				setIsSticky(isScrolled);
-				document.documentElement.classList.toggle('scrolled', isScrolled);
-			}
-	
-			window.addEventListener('scroll', handleScroll)
-			
-			return () => {
-				window.removeEventListener('scroll', handleScroll);
-			}
+		const handleScroll = () => {
+			const scrollThreshold = 100
+			const isScrolled = window.scrollY > scrollThreshold
+			setIsSticky(isScrolled)
+			document.documentElement.classList.toggle('scrolled', isScrolled)
+		}
+
+		window.addEventListener('scroll', handleScroll)
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll)
+		}
 	}, [])
 
 	return (
 		<>
-			<header className={styles.header}>
-				<Header />
-				<NavBar isSticky={isSticky} />
-			</header>
+			<Header isSticky={isSticky} />
 			<main>{children}</main>
 			<Footer />
 		</>
