311993c9e2eeed586c670629c4ef28adac457717
Spencer-Breding <Spencerbreding@gmail.com>
2023-08-04 04:35:30 -0700

    Cleaned Up Seasonal item bar on landing page

--- components/seasonalItem.js
+++ components/seasonalItem.js
@@ -2,37 +2,42 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/SeasonalItem.module.css'
-import seasonalOne from '../public/images/seasonal1.jpeg'
+import cup from '../public/images/cup.jpg'
 import seasonalTwo from '../public/images/seasonal2.jpeg'
+import coffee from '../public/images/coffee.jpeg'
 
 const SeasonalItem = () => {
-	return (
-		<React.Fragment>
-			<div className={styles.Container}>
-				<div className={styles.header}>
-					<h2>This Season&apos;s Items</h2>
-					<button>
-						<Link href='/Menu'>Browse Menu</Link>
-					</button>
-				</div>
+    return (
+        <React.Fragment>
+            <div className={styles.Container}>
+                <div className={styles.header}>
+                    <Image
+                        src={seasonalTwo}
+                        alt='seasonal item'
+                        width={464}
+                        height={464 }
+                    />
+                    <h2>Check out our Seasonal Hits!</h2>
+                    <button>
+                        <Link href='/Menu'>Browse Menu</Link>
+                    </button>
+                </div>
 
-				<div className={styles.ImageContainer}>
-					<Image
-						src={seasonalOne}
-						alt='seasonalItem'
-						width={450}
-						height={450}
-					/>
-					<Image
-						src={seasonalTwo}
-						alt='seasonalItem'
-						width={450}
-						height={450}
-					/>
-				</div>
-			</div>
-		</React.Fragment>
-	)
+                <Image
+                    src={cup}
+                    alt='seasonalItem'
+                    width={464}
+                    height={464}
+                />
+                <Image
+                    src={coffee}
+                    alt='seasonalItem'
+                    width={464}
+                    height={464}
+                />
+            </div>
+        </React.Fragment>
+    )
 }
 
 export default SeasonalItem
