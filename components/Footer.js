048f2e41668eeeb937defec34ceb4dbc26b60e7e
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2023-08-08 16:52:32 -0700

    micro animation for newsletter button

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/Footer.module.css'
 import footerCoffeeIcon from '../public/images/footerCoffeeIcon.png'
 import Image from 'next/image'
