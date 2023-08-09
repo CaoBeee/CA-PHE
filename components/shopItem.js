e893b7823b37f9871ce75d893f706fd9bc4663bc
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2023-08-09 12:52:48 -0700

    build error fix

--- components/shopItem.js
+++ components/shopItem.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React from 'react'
 import Image from 'next/image'
 import styles from '../styles/ShopItem.module.css'
 import PropTypes from 'prop-types'
 
-export default function ShopItem({ picture, name, price, category
-}) {
-    return (
-        <div className={styles.container}>
-            <Image width="550" height="350" src={picture} />
-            <p className={styles.item_name}>{name}</p>
-            <p className={styles.price}>{price}</p>
-        </div>
-        );
+export default function ShopItem({ picture, name, price }) {
+	return (
+		<div className={styles.container}>
+			<Image
+				width='550'
+				height='350'
+				src={picture}
+			/>
+			<p className={styles.item_name}>{name}</p>
+			<p className={styles.price}>{price}</p>
+		</div>
+	)
 }
 
 ShopItem.propTypes = {
-    picture: PropTypes.object.isRequired,
-    name: PropTypes.string.isRequired,
-    price: PropTypes.string.isRequired,
-    category: PropTypes.string.isRequired,
-  };
+	picture: PropTypes.object.isRequired,
+	name: PropTypes.string.isRequired,
+	price: PropTypes.string.isRequired,
+	category: PropTypes.string.isRequired,
+}
