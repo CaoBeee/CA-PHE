ce200b3b0cddda3da467c789116c8531ad9bccce
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2023-08-10 19:07:12 -0700

    redid contact page

--- components/contact.js
+++ components/contact.js
@@ -1,22 +1,88 @@
 import React from 'react'
 import styles from '../styles/Contact.module.css'
+import Image from 'next/image'
+import green from '../public/images/green-circle.png'
+import brown from '../public/images/brown-circle.png'
 
 const Contact = () => {
 	return (
-		<div className={styles.contact}>
-			<h1>How you can contact us:</h1>
-			<p>
-				<span>Email:</span> exampleEmail@gmail.com
-			</p>
-			<p>
-				<span>Store Number:</span> 000-000-0000
-			</p>
-			<p>
-				<span>Locations:</span>
-			</p>
-			<p>123 ABC Street</p>
-			<p>SoCal, CA 00000</p>
-		</div>
+		<>
+			<div className={styles.main}>
+				<Image
+					id={styles.green}
+					src={green}
+				/>
+				<div className={styles.contactUsContainer}>
+					<h1>How you can contact us:</h1>
+					<p>
+						<span>Email:</span> exampleEmail@gmail.com
+					</p>
+					<p>
+						<span>Store Number:</span> 000-000-0000
+					</p>
+					<p>
+						<span>Locations:</span>
+					</p>
+					<p>123 ABC Street</p>
+					<p>SoCal, CA 00000</p>
+				</div>
+				<div className={styles.contactFormContainer}>
+					<h1>Want to let us know something about the store?</h1>
+					<form
+						id='contact-form'
+						action=''
+					>
+						<fieldset>
+							<label
+								htmlFor='name'
+								id='name-label'
+							>
+								<input
+									id='name'
+									type='text'
+									placeholder='Your Name'
+									name='name'
+									required
+								/>
+							</label>
+							<label
+								htmlFor='email'
+								id='email-label'
+							>
+								<input
+									id='email'
+									type='email'
+									placeholder='Your Email'
+									name='email'
+									required
+								></input>
+							</label>
+							<label
+								htmlFor='feedback'
+								id='feedback-label'
+							>
+								<textarea
+									id='feedback'
+									type='textarea'
+									placeholder='We would love to hear from our community so if you have any positive feedback or concerns send us a message here or walk in and ask with our team'
+									name='feedback'
+									required
+								></textarea>
+							</label>
+							<input
+								id='submit'
+								type='submit'
+								value='Send Message'
+							/>
+						</fieldset>
+					</form>
+				</div>
+				<Image
+					id={styles.brown}
+					src={brown}
+				/>
+			</div>
+		</>
 	)
 }
 
--- components/contactform.js
+++ components/contactform.js
@@ -3,63 +3,7 @@ import styles from '../styles/Contact.module.css'
 
 const ContactForm = () => {
 	return (
-		<div className={styles.contact}>
-			<h1 id={styles.form_header }>Want to let us know something about the store?</h1>
-			<form
-				id='contact-form'
-				action=''
-			>
-				<fieldset>
-					<p>
-						<label
-							htmlFor='name'
-							id='name-label'
-						>
-							<input
-								id='name'
-								type='text'
-								placeholder='Your Name'
-								name='name'
-								required
-							></input>
-						</label>
-					</p>
-					<p>
-						<label
-							htmlFor='email'
-							id='email-label'
-						>
-							<input
-								id='email'
-								type='email'
-								placeholder='Your Email'
-								name='email'
-								required
-							></input>
-						</label>
-					</p>
-					<p>
-						<label
-							htmlFor='feedback'
-							id='feedback-label'
-						>
-							<textarea
-								id='feedback'
-								type='textarea'
-								placeholder='We would love to hear from our community so if you have any positive feedback or concerns send us a message here or walk in and ask with our team'
-								name='feedback'
-								required
-							></textarea>
-						</label>
-					</p>
-					<input
-						id='submit'
-						type='submit'
-						value='Send Message'
-					/>
-				</fieldset>
-			</form>
-		</div>
+		
 	)
 }
 
